45ff03554030b54867a531997c16f356e2962452
Loki Lust <support@domainsofcanada.ca>
2021-11-28 22:30:36 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -11,50 +11,48 @@ old_kernels=($(dpkg -l | grep linux-image | tr -s " " | \
 
 if [ "${#old_kernels[@]}" -eq 0 ]; then
     echo "No old kernels found"
-    exit 0
 fi
 
 if ! apt-get remove -y --purge "${old_kernels[@]}"; then
     echo "Failed to remove old kernels"
-    exit 1
 fi
 
 # Cleanup package database
 apt -y autoremove 2>&1 | grep -Ev "^$"
 apt -y --fix-broken install 2>&1 | grep -Ev "^$"
-echo -e $TEXT_YELLOW
+echo -e "$TEXT_YELLOW"
 echo 'APT Cleanup package database finished...'
-echo -e $TEXT_RESET
+echo -e "$TEXT_RESET"
 
 
 sudo apt-get update
-echo -e $TEXT_YELLOW
+echo -e "$TEXT_YELLOW"
 echo 'APT update finished...'
-echo -e $TEXT_RESET
+echo -e "$TEXT_RESET"
 
 sudo apt-get dist-upgrade
-echo -e $TEXT_YELLOW
+echo -e "$TEXT_YELLOW"
 echo 'APT distributive upgrade finished...'
-echo -e $TEXT_RESET
+echo -e "$TEXT_RESET"
 
 sudo apt-get upgrade
-echo -e $TEXT_YELLOW
+echo -e "$TEXT_YELLOW"
 echo 'APT upgrade finished...'
-echo -e $TEXT_RESET
+echo -e "$TEXT_RESET"
 
 sudo do-release-upgrade
-echo -e $TEXT_YELLOW
+echo -e "$TEXT_YELLOW"
 echo 'do-release-upgrade finished...'
-echo -e $TEXT_RESET
+echo -e "$TEXT_RESET"
 
 if [ -f /var/run/reboot-required ]; then
-    echo -e $TEXT_RED_B
+    echo -e "$TEXT_RED_B"
     echo 'Reboot required!'
-    echo -e $TEXT_RESET
+    echo -e "$TEXT_RESET"
 else
-    echo -e $TEXT_YELLOW
+    echo -e "$TEXT_YELLOW"
     echo 'No reboot required'
-    echo -e $TEXT_RESET
+    echo -e "$TEXT_RESET"
 fi
 
 exit 0
